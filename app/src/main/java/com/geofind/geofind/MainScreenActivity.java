508dbe4003dfec46803b7f0e69972fec35606738
Mickey <mickey946@gmail.com>
2014-09-29 19:03:00 +0300

    Added hunt list activity with one empty square card view (to see if it works)

--- app/src/main/java/com/geofind/geofind/MainScreenActivity.java
+++ app/src/main/java/com/geofind/geofind/MainScreenActivity.java
@@ -1,6 +1,7 @@
 package com.geofind.geofind;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -13,7 +14,17 @@ public class MainScreenActivity extends Activity {
         setContentView(R.layout.activity_main_screen);
     }
 
+    /**
+     * Start HuntListActivity so the user can choose a hunt to play.
+     *
+     * @param view The current view.
+     */
     public void openHuntList(View view) {
-
+        Intent intent = new Intent(this, HuntListActivity.class);
+        startActivity(intent);
     }
+
+    //TODO: Create new Hunt button
+
+    //TODO: All of Google Games buttons
 }
