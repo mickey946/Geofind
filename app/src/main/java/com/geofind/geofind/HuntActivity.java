ee89974636db6cc7c968ca8021259a9b43bac331
Mickey <mickey946@gmail.com>
2014-10-04 02:27:12 +0300

    In hunt activity: the sliding up panel now pushes the action bar up. Pressing on the back button when the panel is opened would close it. Otherwise, it would go back as usual.

--- app/src/main/java/com/geofind/geofind/HuntActivity.java
+++ app/src/main/java/com/geofind/geofind/HuntActivity.java
@@ -1,17 +1,60 @@
 package com.geofind.geofind;
 
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+
+import com.nineoldandroids.view.animation.AnimatorProxy;
+import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 
 
 public class HuntActivity extends Activity {
+    private static final String TAG = "HuntActivity";
+
+    private SlidingUpPanelLayout mLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR_OVERLAY);
         setContentView(R.layout.activity_hunt);
+
+        mLayout = (SlidingUpPanelLayout) findViewById(R.id.sliding_layout);
+        mLayout.setPanelSlideListener(new SlidingUpPanelLayout.PanelSlideListener() {
+            @Override
+            public void onPanelSlide(View panel, float slideOffset) {
+                Log.i(TAG, "onPanelSlide, offset " + slideOffset);
+                setActionBarTranslation(mLayout.getCurrentParalaxOffset());
+            }
+
+            @Override
+            public void onPanelExpanded(View panel) {
+                Log.i(TAG, "onPanelExpanded");
+            }
+
+            @Override
+            public void onPanelCollapsed(View panel) {
+                Log.i(TAG, "onPanelCollapsed");
+
+            }
+
+            @Override
+            public void onPanelAnchored(View panel) {
+                Log.i(TAG, "onPanelAnchored");
+            }
+
+            @Override
+            public void onPanelHidden(View panel) {
+                Log.i(TAG, "onPanelHidden");
+            }
+        });
     }
 
 
@@ -32,5 +75,57 @@ public class HuntActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Get the height of the action bar for animation.
+     *
+     * @return the height of the action bar.
+     */
+    private int getActionBarHeight() {
+        int actionBarHeight = 0;
+        TypedValue tv = new TypedValue();
+        if (getTheme().resolveAttribute(android.R.attr.actionBarSize, tv, true)) {
+            actionBarHeight = TypedValue.complexToDimensionPixelSize(tv.data,
+                    getResources().getDisplayMetrics());
+        }
+        return actionBarHeight;
+    }
+
+    /**
+     * Translate the action bar up as the user slides up the bottom panel.
+     *
+     * @param y The height of the translation.
+     */
+    public void setActionBarTranslation(float y) {
+        // Figure out the actionbar height
+        int actionBarHeight = getActionBarHeight();
+        // A hack to add the translation to the action bar
+        ViewGroup content = ((ViewGroup) findViewById(android.R.id.content).getParent());
+        int children = content.getChildCount();
+        for (int i = 0; i < children; i++) {
+            View child = content.getChildAt(i);
+            if (child.getId() != android.R.id.content) {
+                if (y <= -actionBarHeight) {
+                    child.setVisibility(View.GONE);
+                } else {
+                    child.setVisibility(View.VISIBLE);
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+                        child.setTranslationY(y);
+                    } else {
+                        AnimatorProxy.wrap(child).setTranslationY(y);
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mLayout != null && mLayout.isPanelExpanded() || mLayout.isPanelAnchored()) {
+            mLayout.collapsePanel(); // collapse the panel if opened (on back press)
+        } else {
+            super.onBackPressed(); // if the panel is collapsed, proceed as usual
+        }
     }
 }
