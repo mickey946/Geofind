8277fb50fdcf30ffb5c8ad2dda300650e34fe0b2
Ilia Marin <ilia.marin@mail.huji.ac.il>
2014-10-04 19:35:12 +0300

    Textual display of the current location (ability to display given location on the map)

--- app/src/main/java/com/geofind/geofind/SplashScreenActivity.java
+++ app/src/main/java/com/geofind/geofind/SplashScreenActivity.java
@@ -1,17 +1,70 @@
 package com.geofind.geofind;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
 
-public class SplashScreenActivity extends Activity {
+public class SplashScreenActivity extends Activity implements LocationListener {
 
+    private GoogleMap mMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        // Getting Google Play availability status
+        int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getBaseContext());
+        if (status != ConnectionResult.SUCCESS){
+            int requestCode = 10;
+            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(status,this,requestCode);
+            dialog.show();
+        }
+        if (mMap == null)
+        {
+            mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+            if (mMap == null)
+                Toast.makeText(this,"Error creating map",Toast.LENGTH_LONG);
+        }
+        mMap.setMyLocationEnabled(true);
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        Criteria criteria = new Criteria();
+        String provider = locationManager.getBestProvider(criteria,true);
+        Location location = locationManager.getLastKnownLocation(provider);
+
+        if (location != null){
+            onLocationChanged(location);
+        }
+
+        locationManager.requestLocationUpdates(provider,20000,0,this);
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mMap == null)
+        {
+            mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+            if (mMap == null)
+                Toast.makeText(this,"Error creating map",Toast.LENGTH_LONG);
+        }
     }
 
 
@@ -29,8 +82,38 @@ public class SplashScreenActivity extends Activity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
+
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        TextView tvLocation = (TextView) findViewById(R.id.tv_location);
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
+
+        LatLng latLng = new LatLng(latitude,longitude);
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+
+        tvLocation.setText("Lat: " + latitude + " Long:" + longitude);
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
     }
 }
