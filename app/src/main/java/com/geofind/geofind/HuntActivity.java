e80d57db5c4945f27453d37d395b41c353e90066
Mickey <mickey946@gmail.com>
2014-10-23 03:47:09 +0300

    Fixed hiding the action bar when sliding up the panel. Now the action bar hides more naturally (only when the sliding panel reached a certain point the actionbar hides quickly), like other Google apps.

--- app/src/main/java/com/geofind/geofind/HuntActivity.java
+++ app/src/main/java/com/geofind/geofind/HuntActivity.java
@@ -1,24 +1,20 @@
 package com.geofind.geofind;
 
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
-import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.view.Window;
 import android.widget.RelativeLayout;
 
 import com.google.android.gms.maps.MapFragment;
-import com.nineoldandroids.view.animation.AnimatorProxy;
 import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 
 import java.util.ArrayList;
@@ -77,7 +73,11 @@ public class HuntActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         getWindow().requestFeature(Window.FEATURE_ACTION_BAR_OVERLAY);
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_hunt);
+
+        View layout = findViewById(R.id.main_content);
+        layout.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
 
         setUpHunt();
 
@@ -111,7 +111,12 @@ public class HuntActivity extends ActionBarActivity {
             @Override
             public void onPanelSlide(View panel, float slideOffset) {
                 Log.i(TAG, "onPanelSlide, offset " + slideOffset);
-                // setActionBarTranslation(slidingUpPanel.getCurrentParalaxOffset());
+                android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+                if (slideOffset >= SLIDING_UP_PANEL_ANCHOR_POINT - 0.01f) {
+                    actionBar.hide();
+                } else {
+                    actionBar.show();
+                }
             }
 
             @Override
@@ -292,49 +297,6 @@ public class HuntActivity extends ActionBarActivity {
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
-        }
-    }
-
-    /**
-     * Get the height of the action bar for animation.
-     *
-     * @return the height of the action bar.
-     */
-    private int getActionBarHeight() {
-        int actionBarHeight = 0;
-        TypedValue tv = new TypedValue();
-        if (getTheme().resolveAttribute(android.R.attr.actionBarSize, tv, true)) {
-            actionBarHeight = TypedValue.complexToDimensionPixelSize(tv.data,
-                    getResources().getDisplayMetrics());
-        }
-        return actionBarHeight;
-    }
-
-    /**
-     * Translate the action bar up as the user slides up the bottom panel.
-     *
-     * @param y The height of the translation.
-     */
-    public void setActionBarTranslation(float y) {
-        // Figure out the actionbar height
-        int actionBarHeight = getActionBarHeight();
-        // A hack to add the translation to the action bar
-        ViewGroup content = ((ViewGroup) findViewById(android.R.id.content).getParent());
-        int children = content.getChildCount();
-        for (int i = 0; i < children; i++) {
-            View child = content.getChildAt(i);
-            if (child.getId() != android.R.id.content) {
-                if (y <= -actionBarHeight) {
-                    child.setVisibility(View.GONE);
-                } else {
-                    child.setVisibility(View.VISIBLE);
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-                        child.setTranslationY(y);
-                    } else {
-                        AnimatorProxy.wrap(child).setTranslationY(y);
-                    }
-                }
-            }
         }
     }
 
