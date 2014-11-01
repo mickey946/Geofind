78156231d8ac28d28e5b65f1d05ef5db04855f3f
Ilia Marin <ilia.marin@mail.huji.ac.il>
2014-11-02 00:42:28 +0200

    Change tracker to be configured by the user preferences

--- app/src/main/java/com/geofind/geofind/GeoFindApp.java
+++ app/src/main/java/com/geofind/geofind/GeoFindApp.java
@@ -1,6 +1,8 @@
 package com.geofind.geofind;
 
 import android.app.Application;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
 import com.google.android.gms.analytics.GoogleAnalytics;
 import com.google.android.gms.analytics.Tracker;
@@ -13,7 +15,7 @@ import java.util.HashMap;
 public class GeoFindApp extends Application {
 
     // The following line should be changed to include the correct property id.
-    private static final String PROPERTY_ID = "UA-XXXXX-Y";
+    private static final String PROPERTY_ID = "UA-56204480-2";
 
     /**
      * Enum used to identify the tracker that needs to be used for tracking.
@@ -33,11 +35,24 @@ public class GeoFindApp extends Application {
     public GeoFindApp() {
         super();
     }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        Tracker tracker = getTracker(TrackerName.GLOBAL_TRACKER);
+        tracker.enableAutoActivityTracking(sharedPreferences.getBoolean(
+                getString(R.string.pref_analytics),true));
+        tracker.enableExceptionReporting(sharedPreferences.getBoolean(
+                getString(R.string.pref_crash_logs),true));
+        tracker.setSessionTimeout(300);
+    }
+
     synchronized Tracker getTracker(TrackerName trackerId) {
         if (!mTrackers.containsKey(trackerId)) {
 
             GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
-            Tracker t = analytics.newTracker(R.xml.global_tracker);
+            Tracker t = analytics.newTracker(PROPERTY_ID);
             mTrackers.put(trackerId, t);
 
         }
