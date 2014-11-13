0a5982fc15438d0891b6b7087998a42a26f2720c
tzaf <tzafrir.harazy@mail.huji.ac.il>
2014-11-13 17:41:49 +0200

    Added creatorID field to Comment class.

--- app/src/main/java/com/geofind/geofind/Comment.java
+++ app/src/main/java/com/geofind/geofind/Comment.java
@@ -15,18 +15,23 @@ public class Comment implements Serializable {
     private static final String PARSE_TITLE_FIELD = "title";
     private static final String PARSE_REVIEW_FIELD = "review";
     private static final String PARSE_RATING_FIELD = "rating";
+    private static final String PARSE_USER_ID_FIELD = "userID";
+
 
 
     private String _title;
     private String _review;
     private float _rating;
     private Date _dateCreated;
+    private String _creatorID;
 
 
     public Comment(String title, String review, float rating) {
         _title = title;
         _review = review;
         _rating = rating;
+        //TODO change "creatorID"
+        _creatorID = "creatorID";
     }
 
     public Comment(ParseObject remoteComment) {
@@ -34,6 +39,7 @@ public class Comment implements Serializable {
         _review = remoteComment.getString(PARSE_REVIEW_FIELD);
         _rating = (float) remoteComment.getDouble(PARSE_RATING_FIELD);
         _dateCreated = remoteComment.getCreatedAt();
+        _creatorID = remoteComment.getString(PARSE_USER_ID_FIELD);
     }
 
     public String getTitle() {
@@ -52,11 +58,16 @@ public class Comment implements Serializable {
         return _dateCreated;
     }
 
+    public String getCreatorID() {
+        return _creatorID;
+    }
+
     public ParseObject toParseObject() {
         ParseObject remoteComment = new ParseObject(PARSE_CLASS_NAME);
         remoteComment.put(PARSE_TITLE_FIELD, _title);
         remoteComment.put(PARSE_REVIEW_FIELD, _review);
         remoteComment.put(PARSE_RATING_FIELD, _rating);
+        remoteComment.put(PARSE_USER_ID_FIELD, _creatorID);
 
         return remoteComment;
     }
