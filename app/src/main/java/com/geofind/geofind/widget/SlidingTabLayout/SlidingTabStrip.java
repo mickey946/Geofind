bb9770d79168f719bddcccfa34bf0bd4cc8a47db
mickey946 <mickey946@gmail.com>
2014-11-23 19:33:00 +0200

    Signed out users cannot review finished hunts.

--- app/src/main/java/com/geofind/geofind/widget/SlidingTabLayout/SlidingTabStrip.java
+++ app/src/main/java/com/geofind/geofind/widget/SlidingTabLayout/SlidingTabStrip.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.geofind.geofind.widget.SlidingTabLayoutl;
+package com.geofind.geofind.widget.SlidingTabLayout;
 
 import android.R;
 import android.content.Context;
