a64468d67b0d4f4860c52deaef12e820b270fdf6
tzaf <tzafrir.harazy@mail.huji.ac.il>
2014-11-24 21:19:45 +0200

    Documented structures package.

--- app/src/main/java/com/geofind/geofind/structures/Comment.java
+++ app/src/main/java/com/geofind/geofind/structures/Comment.java
@@ -8,11 +8,13 @@ import java.io.Serializable;
 import java.util.Date;
 
 /**
- * Created by Tzafrir on 31/10/2014.
+ * An Object that represents a user's Comment(review) on a Geofind Hunt.
  */
 
 public class Comment implements Serializable {
 
+
+    //String constants for Parse.com class and fields names.
     private static final String PARSE_CLASS_NAME = "Comment";
     private static final String PARSE_TITLE_FIELD = "title";
     private static final String PARSE_REVIEW_FIELD = "review";
@@ -20,14 +22,40 @@ public class Comment implements Serializable {
     private static final String PARSE_USER_ID_FIELD = "userID";
 
 
-
+    /**
+     * The title of this comment.
+     */
     private String _title;
+
+    /**
+     * The body of this comment.
+     */
     private String _review;
+
+    /**
+     * The rating that the commenter gave to the hunt on which he commented(1-5 stars).
+     */
     private float _rating;
+
+    /**
+     * The date this comment was created.
+     */
     private Date _dateCreated;
+
+    /**
+     * The Google id of the commenter who created this comment.
+     */
     private String _creatorID;
 
-
+    /**
+     * Constructor.
+     *
+     * @param title           - the title of this comment.
+     * @param review          - the body of this comment.
+     * @param rating          - the rating of this comment.
+     * @param googleApiClient - the {@link com.google.android.gms.common.api.GoogleApiClient} object to
+     *                        extract the google id of the current user.
+     */
     public Comment(String title, String review, float rating, GoogleApiClient googleApiClient) {
         _title = title;
         _review = review;
@@ -35,6 +63,12 @@ public class Comment implements Serializable {
         _creatorID =  Plus.PeopleApi.getCurrentPerson(googleApiClient).getId();
     }
 
+    /**
+     * Constructor.
+     *
+     * @param remoteComment - The {@link com.parse.ParseObject} representation of this comment,
+     *                      which was received from parse.com database.
+     */
     public Comment(ParseObject remoteComment) {
         _title = remoteComment.getString(PARSE_TITLE_FIELD);
         _review = remoteComment.getString(PARSE_REVIEW_FIELD);
@@ -43,26 +77,50 @@ public class Comment implements Serializable {
         _creatorID = remoteComment.getString(PARSE_USER_ID_FIELD);
     }
 
+    /**
+     * Returns this comment's title.
+     * @return - this comment's title.
+     */
     public String getTitle() {
         return _title;
     }
 
+    /**
+     * Return this comment's body.
+     * @return - this comment's body.
+     */
     public String getReview() {
         return _review;
     }
 
+    /**
+     * Returns this comment's rating.
+     * @return - this comment's rating.
+     */
     public float getRating() {
         return _rating;
     }
 
+    /**
+     * Returns this comment's creation date.
+     * @return - this comment's creation date.
+     */
     public Date getDateCreated() {
         return _dateCreated;
     }
 
+    /**
+     * Returns the google id of the user who created this comment.
+     * @return - the google id of the user who created this comment.
+     */
     public String getCreatorID() {
         return _creatorID;
     }
 
+    /**
+     * Returns a {@link com.parse.ParseObject} representation of this comment.
+     * @return - a {@link com.parse.ParseObject} representation of this comment.
+     */
     public ParseObject toParseObject() {
         ParseObject remoteComment = new ParseObject(PARSE_CLASS_NAME);
         remoteComment.put(PARSE_TITLE_FIELD, _title);
@@ -72,6 +130,5 @@ public class Comment implements Serializable {
 
         return remoteComment;
     }
-
 
 }
--- app/src/main/java/com/geofind/geofind/structures/Point.java
+++ app/src/main/java/com/geofind/geofind/structures/Point.java
@@ -12,45 +12,82 @@ import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
 /**
- * Created by Ilia Marin on 08/10/2014.
+ * An object which represents a point in a Geofind hunt.
  */
 public class Point implements Serializable {
 
+    /**
+     * The latitude of this point.
+     */
     private  double _latitude;
+
+    /**
+     * The longitude of this point.
+     */
     private  double _longitude;
 
+    /**
+     * Constructor.
+     */
     public Point(double latitude, double longitude) {
         _latitude = latitude;
         _longitude = longitude;
     }
 
+    /**
+     * Constructor.
+     */
     public Point(LatLng latLng) {
         _latitude = latLng.latitude;
         _longitude = latLng.longitude;
     }
 
+    /**
+     * Constructor.
+     */
     public Point(Location location) {
         _latitude = location.getLatitude();
         _longitude = location.getLongitude();
     }
 
+    /**
+     * Constructor.
+     */
     public Point(ParseGeoPoint parseGeoPoint) {
         _latitude = parseGeoPoint.getLatitude();
         _longitude = parseGeoPoint.getLongitude();
     }
 
+    /**
+     * Returns this point's latitude.
+     *
+     * @return - this point's latitude.
+     */
     public double getLatitude() {
         return _latitude;
     }
 
+    /**
+     * Returns this point's longitude.
+     *
+     * @return - this point's longitude.
+     */
     public double getLongitude() {
         return _longitude;
     }
 
+    /**
+     * returns a {@link com.google.android.gms.maps.model.LatLng} object representation of this point.
+     * @return - a {@link com.google.android.gms.maps.model.LatLng} object representation of this point.
+     */
     public LatLng toLatLng(){
         return  new LatLng(_latitude,_longitude);
     }
 
+    /**
+     * Returns a {@link android.location.Location} object representation of this point.
+     * @return - a {@link android.location.Location} object representation of this point.
+     */
     public Location toLocation(){
         Location l = new Location(LocationManager.PASSIVE_PROVIDER);
         l.setLatitude(_latitude);
@@ -58,16 +95,26 @@ public class Point implements Serializable {
         return  l;
     }
 
+    /**
+     * Returns a {@link com.parse.ParseGeoPoint} representation of this point.
+     * @return - a {@link com.parse.ParseGeoPoint} representation of this point.
+     */
     public ParseGeoPoint toParseGeoPoint() {
         return new ParseGeoPoint(_latitude, _longitude);
     }
 
+    /**
+     * For implementing serializable.
+     */
     private void writeObject(ObjectOutputStream out) throws IOException {
         out.defaultWriteObject();
         out.writeDouble(_latitude);
         out.writeDouble(_longitude);
     }
 
+    /**
+     * For implementing Serializable.
+     */
     private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
         in.defaultReadObject();
         _latitude = in.readDouble();
